04b9145b3db4389210344a10f97713e17ecca6b7
NKey <n.kirbitov@mail.ru>
2024-06-28 20:39:03 +0300

    register-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
 
 function App() {
+
+  const [formData, setFormData] = React.useState({
+    email: '',
+    fullName:'',
+    password:'',
+    password2:'',
+    remember: false,
+  });
+  const [errors, setErrors] = React.useState({});
+
+  const handleChange = (e) => {
+    const {name, value, type, checked} = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = validateForm();
+    if (Object.keys(newErrors).length === 0) {
+      console.log('Form submitted', formData);
+    }else {
+      setErrors(newErrors);
+    }
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.email) newErrors.email = 'Введите email';
+    if (!formData.fullName) newErrors.fullName = 'Введите полное имя';
+    if (!formData.password) newErrors.password = 'Введите пароль';
+    if (!formData.password !== formData.password2) newErrors.password2 = 'Пароли не совпадают';
+    return newErrors;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="banner">
+        <div className="banner__inner">
+          <img src="/chat.png" alt="Chat logo"/>
+          <h2>Общайся с друзьями</h2>
+          <p>Быстрый и удобный чат для общения с друзьями или коолегами по работе</p>
+        </div>
+      </div>
+      <div className="form">
+        <form className="form__content" onSubmit={handleSubmit}>
+          <h2>Регистрация</h2>
+          <p>Для того, чтобы войти в чат, необходимо произвести авторизоваться</p>
+          <div className={`form__field ${errors.email ? 'error' : ''}`}>
+            <input 
+              type="text" 
+              name="email" 
+              placeholder="E-Mail"
+              value={formData.email}
+              onChange={handleChange} 
+            />
+            {errors.email && <span className='error-message'>{errors.email}</span>}
+            <svg className="check" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm5.707,8.707-7,7a1,1,0,0,1-1.414,0l-3-3a1,1,0,0,1,1.414-1.414L10,14.586l6.293-6.293a1,1,0,0,1,1.414,1.414Z" />
+            </svg>
+          </div>
+          <div className={`form__field ${errors.fullName ? 'error' : ''}`}>
+            <input 
+              type="text" 
+              name="fullName" 
+              placeholder="Полное имя" 
+              value={formData.fullName}
+              onChange={handleChange}
+            />
+            {errors.fullName && <span className='error-message'>{errors.fullName}</span>}
+          </div>
+          <div className={`form__field ${errors.password ? 'error' : ''}`}>
+            <input 
+              type="password" 
+              name="password" 
+              placeholder="Пароль"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            {errors.password && <span className='error-message'>{errors.password}</span>}
+            <svg className="eye" height="24px" viewBox="0 0 24 24" width="24px">
+              <path d="M21.821,12.43c-0.083-0.119-2.062-2.944-4.793-4.875C15.612,6.552,13.826,6,12,6c-1.825,0-3.611,0.552-5.03,1.555  c-2.731,1.931-4.708,4.756-4.791,4.875c-0.238,0.343-0.238,0.798,0,1.141c0.083,0.119,2.06,2.944,4.791,4.875  C8.389,19.448,10.175,20,12,20c1.826,0,3.612-0.552,5.028-1.555c2.731-1.931,4.71-4.756,4.793-4.875  C22.06,13.228,22.06,12.772,21.821,12.43z M12,16.5c-1.934,0-3.5-1.57-3.5-3.5c0-1.934,1.566-3.5,3.5-3.5c1.93,0,3.5,1.566,3.5,3.5  C15.5,14.93,13.93,16.5,12,16.5z" />
+              <g>
+                <path d="M14,13c0,1.102-0.898,2-2,2c-1.105,0-2-0.898-2-2c0-1.105,0.895-2,2-2C13.102,11,14,11.895,14,13z" />
+              </g>
+            </svg>
+          </div>
+          <div className={`form__field ${errors.password2 ? 'error' : ''}`}>
+            <input 
+              type="password" 
+              name="password2" 
+              placeholder="Пароль"
+              value={formData.password2}
+              onChange={handleChange}
+            />
+            {errors.password2 && <span className='error-message'>{errors.password2}</span>}
+            <svg className="eye" height="24px" viewBox="0 0 24 24" width="24px">
+              <path d="M21.821,12.43c-0.083-0.119-2.062-2.944-4.793-4.875C15.612,6.552,13.826,6,12,6c-1.825,0-3.611,0.552-5.03,1.555  c-2.731,1.931-4.708,4.756-4.791,4.875c-0.238,0.343-0.238,0.798,0,1.141c0.083,0.119,2.06,2.944,4.791,4.875  C8.389,19.448,10.175,20,12,20c1.826,0,3.612-0.552,5.028-1.555c2.731-1.931,4.71-4.756,4.793-4.875  C22.06,13.228,22.06,12.772,21.821,12.43z M12,16.5c-1.934,0-3.5-1.57-3.5-3.5c0-1.934,1.566-3.5,3.5-3.5c1.93,0,3.5,1.566,3.5,3.5  C15.5,14.93,13.93,16.5,12,16.5z" />
+              <g>
+                <path d="M14,13c0,1.102-0.898,2-2,2c-1.105,0-2-0.898-2-2c0-1.105,0.895-2,2-2C13.102,11,14,11.895,14,13z" />
+              </g>
+            </svg>
+          </div>
+          <div className="form__additional">
+            <div>
+              <input 
+                class="styled-checkbox" 
+                id="checkbox" 
+                type="checkbox"
+                name="remember" 
+                checked={formData.remember}
+                onChange={handleChange}
+              />
+              <label htmlFor="checkbox">Запомнить</label>
+            </div>
+            <a href='/'>Забыл пароль?</a>
+          </div>
+
+          <button type="submit">Войти</button>
+        </form>
+      </div>
     </div>
   );
 }
